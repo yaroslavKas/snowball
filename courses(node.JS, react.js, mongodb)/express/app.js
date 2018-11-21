f2900efbebaa00d55543afd981a69a02f653e5c9
yaroslav <yaroslav@vidax.com>
2018-11-21 10:57:56 +0200

    add express example.

--- courses(node.JS, react.js, mongodb)/express/app.js
+++ courses(node.JS, react.js, mongodb)/express/app.js
@@ -1,16 +1,30 @@
 "use strict";
 
-const express = require("express");
-
+const express = require('express');
+const fs = require('fs');
+const bodyParser = require('body-parser');
 const app = express();
+const qs = require('querystring');
 
-app.use("/calc/:operation/:x/:y", (req, res) =>{
-  let op = req.params["operation"];
-  let x = req.params["x"];
-  let y = req.params["y"];
-  res.send(`Hello ${op}!  ${x} ${y}`);
+app.use("/", express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({extended: false}));
 
+app.get("/user", (req, res) => {
+  fs.readFile("./index.html", "utf8", (err, data) => {
+    data = data.replace("{name}", "User");
+
+    res.end(data);
+  });
 });
 
+app.post("/user", (req, res) => {
+  console.log(req.body);
+
+  let name = req.body.name;
+  let age = req.body.age;
+  res.send(`Name: ${name}, Age: ${age}`);
+});
 
 app.listen(3000);
+
+
